9790b1f54c3b415fc93ab6fe9f553356b765152f
Diego Andrés <diegoandres_cortes@outlook.com>
2019-07-14 21:03:37 -0500

    Alerts txt strings
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Languages/Pokes.english.php
+++ Languages/Pokes.english.php
@@ -13,3 +13,7 @@ $txt['poke_yourself_error'] = 'You cannot send pokes to yourself.';
 $txt['poke_nouser_error'] = 'Unable to find an user.';
 $txt['poke_already_sent'] = 'This user still has a previous poke from you. He will poke you back eventually, or ignore you and you can come back.';
 $txt['poke_not_there'] = 'That poke is no longer there.';
+$txt['alert_group_pokes'] = 'Pokes';
+$txt['alert_poked'] = 'When I receive a poke from someone';
+$txt['alert_poke_poked'] = '{member_link} just sent you a poke';
+$txt['poke_noti_poked'] = 'just sent you a poke';
